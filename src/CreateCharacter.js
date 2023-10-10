e86b01793e3879990460c6f6518995064c0bed52
AfternoonDozer <noahmendoza97@gmail.com>
2023-10-10 16:12:37 -0400

    Updates CreateCharacter.js

--- src/CreateCharacter.js
+++ src/CreateCharacter.js
@@ -4,9 +4,13 @@ function CreateCharacter() {
   const [characterData, setCharacterData] = useState({
     background: '',
     race: '',
-    class: '',
-    // Add more fields as needed
+    characterClass: '',
+    // Add more fields for abilities, proficiencies, etc.
   });
+
+  const [isBackgroundExpanded, setIsBackgroundExpanded] = useState(false);
+  const [isRaceExpanded, setIsRaceExpanded] = useState(false);
+  const [isClassExpanded, setIsClassExpanded] = useState(false);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -16,10 +20,78 @@ function CreateCharacter() {
     });
   };
 
+  const handleButtonClick = (selectedValue, field) => {
+    setCharacterData({
+      ...characterData,
+      [field]: selectedValue,
+    });
+    switch (field) {
+      case 'background':
+        setIsBackgroundExpanded(false);
+        break;
+      case 'race':
+        setIsRaceExpanded(false);
+        break;
+      case 'characterClass':
+        setIsClassExpanded(false);
+        break;
+      default:
+        break;
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     // Send characterData to the server for creation
     // You can use Axios or the fetch API for this purpose
+  };
+
+  const toggleExpansion = (field) => {
+    switch (field) {
+      case 'background':
+        setIsBackgroundExpanded(!isBackgroundExpanded);
+        setIsRaceExpanded(false); // Collapse other sections
+        setIsClassExpanded(false);
+        break;
+      case 'race':
+        setIsRaceExpanded(!isRaceExpanded);
+        setIsBackgroundExpanded(false); // Collapse other sections
+        setIsClassExpanded(false);
+        break;
+      case 'characterClass':
+        setIsClassExpanded(!isClassExpanded);
+        setIsBackgroundExpanded(false); // Collapse other sections
+        setIsRaceExpanded(false);
+        break;
+      default:
+        break;
+    }
+  };
+
+  // Define arrays of options (replace with your actual data)
+  const backgroundOptions = Array.from({ length: 11 }, (_, i) => `Background ${i + 1}`);
+  const raceOptions = Array.from({ length: 8 }, (_, i) => `Race ${i + 1}`);
+  const classOptions = Array.from({ length: 12 }, (_, i) => `Class ${i + 1}`);
+
+  const generateButtonRows = (options, field) => {
+    const rows = [];
+    for (let i = 0; i < Math.ceil(options.length / 4); i++) {
+      const row = (
+        <div key={i} style={{ display: 'flex' }}>
+          {options.slice(i * 4, (i + 1) * 4).map((option) => (
+            <button
+              key={option}
+              onClick={() => handleButtonClick(option, field)}
+              style={{ margin: '4px' }}
+            >
+              {option}
+            </button>
+          ))}
+        </div>
+      );
+      rows.push(row);
+    }
+    return rows;
   };
 
   return (
@@ -27,36 +99,51 @@ function CreateCharacter() {
       <h1>Create Character</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="background">Background:</label>
-          <input
-            type="text"
-            id="background"
-            name="background"
-            value={characterData.background}
-            onChange={handleInputChange}
-          />
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              cursor: 'pointer',
+            }}
+            onClick={() => toggleExpansion('background')}
+          >
+            <span style={{ marginRight: '8px' }}>Background</span>
+            {isBackgroundExpanded ? '▲' : '▼'}
+          </div>
+          {isBackgroundExpanded && <div>{generateButtonRows(backgroundOptions, 'background')}</div>}
         </div>
+
         <div>
-          <label htmlFor="race">Race:</label>
-          <input
-            type="text"
-            id="race"
-            name="race"
-            value={characterData.race}
-            onChange={handleInputChange}
-          />
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              cursor: 'pointer',
+            }}
+            onClick={() => toggleExpansion('race')}
+          >
+            <span style={{ marginRight: '8px' }}>Race</span>
+            {isRaceExpanded ? '▲' : '▼'}
+          </div>
+          {isRaceExpanded && <div>{generateButtonRows(raceOptions, 'race')}</div>}
         </div>
+
         <div>
-          <label htmlFor="class">Class:</label>
-          <input
-            type="text"
-            id="class"
-            name="class"
-            value={characterData.class}
-            onChange={handleInputChange}
-          />
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              cursor: 'pointer',
+            }}
+            onClick={() => toggleExpansion('characterClass')}
+          >
+            <span style={{ marginRight: '8px' }}>Class</span>
+            {isClassExpanded ? '▲' : '▼'}
+          </div>
+          {isClassExpanded && <div>{generateButtonRows(classOptions, 'characterClass')}</div>}
         </div>
-        {/* Add more input fields for other character attributes */}
+
+        {/* Other fields for abilities, proficiencies, etc. */}
         <button type="submit">Create Character</button>
       </form>
     </div>
