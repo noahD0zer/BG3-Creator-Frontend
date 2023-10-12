d104d36ffc031f0ef0b2f0add22850e7ed3758c3
AfternoonDozer <noahmendoza97@gmail.com>
2023-10-11 21:48:30 -0400

    deletes expansion field in Createcharacter.jsx

--- src/CreateCharacter.jsx
+++ src/CreateCharacter.jsx
@@ -9,190 +9,93 @@ function CreateCharacter() {
     // Add more fields for abilities, proficiencies, etc.
   });
 
-  const [isBackgroundExpanded, setIsBackgroundExpanded] = useState(false);
-  const [isRaceExpanded, setIsRaceExpanded] = useState(false);
-  const [isClassExpanded, setIsClassExpanded] = useState(false);
+  const [backgrounds, setBackgrounds] = useState([]);
+  const [classes, setClasses] = useState([]);
+  const [races, setRaces] = useState([]);
 
-  const [backgrounds, setBackgrounds] = useState([]); // To store background data
-  const [classes, setClasses] = useState([]); // To store class data
-  const [races, setRaces] = useState([]); // To store race data
+  useEffect(() => {
+    getBackgrounds()
+      .then(res => {
+        setBackgrounds(res.data);
+        console.log("backgrounds", res.data);
+      })
+      .catch((error) => console.error('Error fetching backgrounds:', error));
+  }, []);
 
-  const handleButtonClick = (selectedValue, field) => {
-    setCharacterData({
-      ...characterData,
-      [field]: selectedValue,
-    });
-    switch (field) {
-      case 'background':
-        setIsBackgroundExpanded(false);
-        break;
-      case 'race':
-        setIsRaceExpanded(false);
-        break;
-      case 'characterClass':
-        setIsClassExpanded(false);
-        break;
-      default:
-        break;
-    }
-  };
+  useEffect(() => {
+    getClasses()
+      .then(res => {
+        setClasses(res.data);
+        console.log("classes", res.data);
+      })
+      .catch((error) => console.error('Error fetching classes:', error));
+  }, []);
+
+  useEffect(() => {
+    getRaces()
+      .then(res => {
+        setRaces(res.data);
+        console.log("races", res.data);
+      })
+      .catch((error) => console.error('Error fetching races:', error));
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Send characterData to the server for creation
-    // use fetch API
+    // Use the fetch API or any other method for this
   };
-
-  const toggleExpansion = (field) => {
-    switch (field) {
-      case 'background':
-        setIsBackgroundExpanded(!isBackgroundExpanded);
-        setIsRaceExpanded(false); // Collapse other sections
-        setIsClassExpanded(false);
-        break;
-      case 'race':
-        setIsRaceExpanded(!isRaceExpanded);
-        setIsBackgroundExpanded(false); // Collapse other sections
-        setIsClassExpanded(false);
-        break;
-      case 'characterClass':
-        setIsClassExpanded(!isClassExpanded);
-        setIsBackgroundExpanded(false); // Collapse other sections
-        setIsRaceExpanded(false);
-        break;
-      default:
-        break;
-    }
-  };
-
-  useEffect(() => {
-    // Fetch background data from your backend API
-      getBackgrounds()
-        .then(res => setBackgrounds(res.data))
-        .catch((error) => console.error('Error fetching backgrounds:', error));
-  }, []);
-  console.log("backgrounds", backgrounds)
-  
-  useEffect(() => {
-    // Fetch class data from backend API
-      getClasses()
-        .then(res => setClasses(res.data))
-        .catch((error) => console.error('Error fetching classes:', error));
-  }, []);
-  console.log("classes", classes)
-  
-  useEffect(() => {
-    // Fetch race data from backend API
-      getRaces()
-        .then(res => setRaces(res.data))
-        .catch((error) => console.error('Error fetching races:', error));
-  }, []);
-  console.log("races",races)
-  
-  // const generateButtonRows = (options, field) => {
-  //   const rows = [];
-  //   for (let i = 0; i < Math.ceil(options.length / 4); i++) {
-  //     const row = (
-  //       <div key={i} style={{ display: 'flex' }}>
-  //         {options.slice(i * 4, (i + 1) * 4).map((option) => (
-  //           <button
-  //             key={option}
-  //             onClick={() => handleButtonClick(option, field)}
-  //             style={{ margin: '4px' }}
-  //           >
-  //             {option}
-  //           </button>
-  //         ))}
-  //       </div>
-  //     );
-  //     rows.push(row);
-  //   }
-  //   return rows;
-  // };
 
   return (
     <div>
       <h1>Create Character</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              cursor: 'pointer',
-            }}
-            onClick={() => toggleExpansion('background')}
+          <label htmlFor="background">Background:</label>
+          <select
+            id="background"
+            value={characterData.background}
+            onChange={(e) => setCharacterData({ ...characterData, background: e.target.value })}
           >
-            <span style={{ marginRight: '8px' }}>Background</span>
-            {isBackgroundExpanded ? '▲' : '▼'}
-          </div>
-          {isBackgroundExpanded && (
-            <select
-              value={characterData.background}
-              onChange={(e) => handleButtonClick(e.target.value, 'background')}
-            >
-              <option value="">Select a Background</option>
-              {backgrounds.map((bg) => (
-                <option key={bg.id} value={bg.background}>
-                  {bg.background}
-                </option>
-              ))}
-            </select>
-          )}
+            <option value="">Select a Background</option>
+            {backgrounds.map((bg) => (
+              <option key={bg.id} value={bg.background}>
+                {bg.background}
+              </option>
+            ))}
+          </select>
         </div>
 
         <div>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              cursor: 'pointer',
-            }}
-            onClick={() => toggleExpansion('race')}
+          <label htmlFor="race">Race:</label>
+          <select
+            id="race"
+            value={characterData.race}
+            onChange={(e) => setCharacterData({ ...characterData, race: e.target.value })}
           >
-            <span style={{ marginRight: '8px' }}>Race</span>
-            {isRaceExpanded ? '▲' : '▼'}
-          </div>
-          {isRaceExpanded && (
-            <select
-              value={characterData.race}
-              onChange={(e) => handleButtonClick(e.target.value, 'race')}
-            >
-              <option value="">Select a Race</option>
-              {races.map((race) => (
-                <option key={race.id} value={race.race}>
-                  {race.race}
-                </option>
-              ))}
-            </select>
-          )}
+            <option value="">Select a Race</option>
+            {races.map((race) => (
+              <option key={race.id} value={race.race}>
+                {race.race}
+              </option>
+            ))}
+          </select>
         </div>
 
         <div>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              cursor: 'pointer',
-            }}
-            onClick={() => toggleExpansion('characterClass')}
+          <label htmlFor="characterClass">Class:</label>
+          <select
+            id="characterClass"
+            value={characterData.characterClass}
+            onChange={(e) => setCharacterData({ ...characterData, characterClass: e.target.value })}
           >
-            <span style={{ marginRight: '8px' }}>Class</span>
-            {isClassExpanded ? '▲' : '▼'}
-          </div>
-          {isClassExpanded && (
-            <select
-              value={characterData.characterClass}
-              onChange={(e) => handleButtonClick(e.target.value, 'characterClass')}
-            >
-              <option value="">Select a Class</option>
-              {classes.map((cls) => (
-                <option key={cls.id} value={cls.class}>
-                  {cls.class}
-                </option>
-              ))}
-            </select>
-          )}
+            <option value="">Select a Class</option>
+            {classes.map((cls) => (
+              <option key={cls.id} value={cls.class}>
+                {cls.class}
+              </option>
+            ))}
+          </select>
         </div>
 
         {/* Other fields for abilities, proficiencies, etc. */}
@@ -203,4 +106,3 @@ function CreateCharacter() {
 }
 
 export default CreateCharacter;
-         
