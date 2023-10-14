9e891bad2a47bb661578711fc0c848206483e802
AfternoonDozer <noahmendoza97@gmail.com>
2023-10-14 19:00:11 -0400

    adds styling

--- src/components/character/CharacterDetail.js
+++ src/components/character/CharacterDetail.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getOneCharacter, updateCharacter } from '../../api/characters';
 import EditCharacter from './EditCharacter';
+import { Button, Card, CardBody, Col, Row, Container, Form, CardTitle, CardText } from 'react-bootstrap'
 
 
 
@@ -32,59 +33,70 @@ const CharacterDetail = ({ user, msgAlert }) => {
   }
 
   return (
-    <div>
-      <h1>Character Details</h1>
-      <p>Name: {character.name}</p>
-      <p>Background: {character.background}</p>
-      <p>Race: {character.race}</p>
-      <p>Class: {character.characterClass}</p>
+    <>
+      <Container className='m-2'>
+        <Row>
+          <Col>
+            <Card className='m-2'>
+              <CardBody>
+                <CardTitle className='fs-2'>{character.name}</CardTitle>
+                <CardText className='ms-3'>Background: {character.background}</CardText>
+                <CardText className='ms-3'>Race: {character.race}</CardText>
+                <CardText className='ms-3'>Class: {character.characterClass}</CardText>
+              </CardBody>
 
-      <div>
-        <h2>Weapon Proficiencies</h2>
-        <ul>
-          {character.weaponProficiencies.map((proficiency, index) => (
-            <li key={index}>{proficiency}</li>
-          ))}
-        </ul>
-      </div>
+              <CardBody>
+                <CardTitle>Weapon Proficiencies</CardTitle>
+                <CardText>
+                  {character.weaponProficiencies.map((proficiency, index) => (
+                    <CardText className='ms-3' key={index}>{proficiency}</CardText>
+                  ))}
+                </CardText>
+              </CardBody>
 
-      <div>
-        <h2>Armor Proficiencies</h2>
-        <ul>
-          {character.armorProficiencies.map((proficiency, index) => (
-            <li key={index}>{proficiency}</li>
-          ))}
-        </ul>
-      </div>
+              <CardBody>
+                <CardTitle>Armor Proficiencies</CardTitle>
+                <CardText>
+                  {character.armorProficiencies.map((proficiency, index) => (
+                    <CardText className='ms-3' key={index}>{proficiency}</CardText>
+                  ))}
+                </CardText>
+              </CardBody>
 
-      <div>
-        <h2>Skill Proficiencies</h2>
-        <ul>
-          {character.skillProficiencies.map((proficiency, index) => (
-            <li key={index}>{proficiency}</li>
-          ))}
-        </ul>
-      </div>
-
-      {/* Expandable section for editing character details */}
-      <div>
-        
-        <button onClick={editModalShow ? handleEditModalClose : handleEditModalShow}>
-          {editModalShow ? 'Close Edit' : 'Edit'}
-        </button>
-
-        {editModalShow && (
-          <EditCharacter
-            user={user}
-            updateCharacter={updateCharacter}
-            msgAlert={msgAlert}
-            handleClose={() => setEditModalShow(false)}
-            triggerRefresh={() => setCharacter(null)}
-            character={character}
-          />
-        )}
-      </div>
-    </div>
+              <CardBody>
+                <CardTitle>Skill Proficiencies</CardTitle>
+                <CardText>
+                  {character.skillProficiencies.map((proficiency, index) => (
+                    <CardText className='ms-3' key={index}>{proficiency}</CardText>
+                  ))}
+                </CardText>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>        
+            <Button className='m-2' onClick={editModalShow ? handleEditModalClose : handleEditModalShow}>
+              {editModalShow ? 'Close Edit' : 'Edit'}
+            </Button>
+      
+            <Form className='m-2'>
+              {editModalShow && (
+                <Card>
+                  <EditCharacter
+                    user={user}
+                    updateCharacter={updateCharacter}
+                    msgAlert={msgAlert}
+                    handleClose={() => setEditModalShow(false)}
+                    triggerRefresh={() => setCharacter(null)}
+                    character={character} 
+                  />
+                </Card>
+              )}
+            </Form>
+          </Col>    
+        </Row>
+      </Container>
+    </>
+    
   );
 };
 
